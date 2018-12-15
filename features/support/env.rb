63a6ff37252d02ecb30c158bfd0cd5c82dbe4a08
Rich Downie <rich@blackboxd.com>
2018-12-15 16:56:38 -0500

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,5 +5,10 @@ Before do
     @browser = Selenium::WebDriver.for :chrome, desired_capabilities: $caps
   end
   @fsar_api_key = ""
+  if @fsar_api_key.empty?
+    puts "1. Please Sign In https://fullstackautomationwithruby.com/users/sign_in."
+    puts "2. Visit your PROFILE page."
+    puts "3. Copy and paste your @fsar_api_key in features/support/env.rb."
+  end
   @domain = 'https://fullstackautomationwithruby.com'
 end
