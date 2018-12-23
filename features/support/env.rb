b032b602a74c498c1e94d6dbb2ed3b90f55e291c
Rich Downie <rich@blackboxd.com>
2018-12-23 12:45:23 -0500

    fixed

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,7 +8,7 @@ Before do
   if @fsar_api_key.empty?
     puts "1. Please Sign In https://fullstackautomationwithruby.com/users/sign_in."
     puts "2. Visit your PROFILE page."
-    puts "3. Copy and paste your @fsar_api_key in features/support/env.rb."
+    puts "3. Copy and paste your @fsar_api_key in features/support/env.rb on line:10"
   end
   @domain = 'https://fullstackautomationwithruby.com'
 end
